0d4e8d79e02fffbbdaac7b966380f761fda77409
Yogeshwaran <yogeshwaranm.doodleblue@gmail.com>
2024-05-29 17:13:37 +0530

    added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,17 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
-  
   def index
     @articles=Article.all
+    @article = Article.new  # Ensure @article is initialized
   end
   def show
     @article = Article.find(params[:id])
   end
- 
-
-  def create
+  
+  def new
+    @article = Article.new#The new action instantiates a new article, but does not save it. 
+  end
+  
+def create
     @article = Article.new(title: "...", body: "...")
 
     if @article.save
@@ -20,7 +20,34 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  private
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+#   The edit action fetches the article from the database, and stores it in @article so that it can be used when building the form. By default, the edit action will render app/views/articles/edit.html.erb.
+
+# The update action (re-)fetches the article from the database, and attempts to update it with the submitted form data filtered by article_params. If no validations fail and the update is successful, the action redirects the browser to the article's page. Else, the action redisplays the form — with error messages — by rendering app/views/articles/edit.html.erb.
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  # The destroy action fetches the article from the database, and calls destroy on it. Then, it redirects the browser to the root path with status code 303
+
+private
     def article_params
       params.require(:article).permit(:title, :body)
     end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,3 +3,6 @@ validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
 end
+# The first validation declares that a title value must be present. Because title is a string, this means that the title value must contain at least one non-whitespace character.
+
+# The second validation declares that a body value must also be present. Additionally, it declares that the body value must be at least 10 characters long.
